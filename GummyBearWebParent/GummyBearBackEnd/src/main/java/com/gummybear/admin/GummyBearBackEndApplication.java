aa906a491e36b3747af22f4eaf3cff3f12332bca
Johaido <jonas.hainmueller@students.fhnw.ch>
2021-04-29 15:58:07 +0200

    Initial Commit

---
+++ GummyBearWebParent/GummyBearBackEnd/src/main/java/com/gummybear/admin/GummyBearBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.gummybear.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GummyBearBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GummyBearBackEndApplication.class, args);
+	}
+
+}
---
+++ GummyBearWebParent/GummyBearFrontEnd/src/main/java/com/gummybear/site/GummyBearFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.gummybear.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GummyBearFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GummyBearFrontEndApplication.class, args);
+	}
+
+}
