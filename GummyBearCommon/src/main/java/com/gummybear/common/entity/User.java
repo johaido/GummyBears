e10d60f55c1973f527c32e550b41d2509486ed86
osolga <olga.osipova@students.fhnw.ch>
2021-05-02 21:22:10 +0200

    Added User class to GummyBearCommon.

---
+++ GummyBearCommon/src/main/java/com/gummybear/common/entity/User.java
@@ -0,0 +1,138 @@
+package com.gummybear.common.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; 
+	
+	@Column(length = 128, nullable = false, unique = true)
+	private String email;
+	
+	@Column(length = 64, nullable = false)
+	private String password;
+	
+	@Column(name = "first_name", length = 45, nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name", length = 45, nullable = false)
+	private String lastName;
+	
+	@Column(name = "job_title", length = 64, nullable = false)
+	private String jobTitle;
+	
+	@Column(name = "working_hours", length = 4, nullable = false)
+	private Double workingHours;
+	
+	private boolean enabled;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "users_roles",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id")
+			)
+	private Set<Role> roles = new HashSet<>();
+
+	public User() {
+	}
+	
+	public User(String email, String password, String firstName, String lastName) {
+		this.email = email;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public void addRole(Role role) {
+		this.roles.add(role);
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public Double getWorkingHours() {
+		return workingHours;
+	}
+
+	public void setWorkingHours(Double workingHours) {
+		this.workingHours = workingHours;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	
+}
