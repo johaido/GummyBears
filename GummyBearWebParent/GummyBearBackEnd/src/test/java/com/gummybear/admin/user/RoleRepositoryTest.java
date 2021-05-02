bf1b187606fb40bd09348771295dc235c9c257c2
osolga <olga.osipova@students.fhnw.ch>
2021-05-02 21:18:42 +0200

    Added RoleRepositoryTest class.

---
+++ GummyBearWebParent/GummyBearBackEnd/src/test/java/com/gummybear/admin/user/RoleRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.gummybear.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.gummybear.common.entity.Role;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class RoleRepositoryTest {
+	
+	@Autowired
+	private RoleRepository repo;
+	
+	@Test
+	public void testCreateFirstRole() {
+		Role roleAdmin = new Role("Admin", "Manage employee accounts");
+		Role savedRole = repo.save(roleAdmin);
+		
+		assertThat(savedRole.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testCreateSeveralRoles() {
+		Role roleAdmin = new Role("Admin", "Manage employee accounts");
+		Role roleEmployee = new Role("Employee", "Company employee");
+		
+		repo.saveAll(List.of(roleAdmin, roleEmployee));
+	}
+}
