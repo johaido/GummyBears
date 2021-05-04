bddcdbbdd46ff67ceeb91441e61246b4e6859117
Thitari Somboon <thitari.somboon@students.fhnw.ch>
2021-05-04 12:56:40 +0200

    UserRepository class updated

--- GummyBearWebParent/GummyBearBackEnd/src/main/java/com/gummybear/admin/user/UserRepository.java
+++ GummyBearWebParent/GummyBearBackEnd/src/main/java/com/gummybear/admin/user/UserRepository.java
@@ -8,4 +8,13 @@ import com.gummybear.common.entity.User;
 @Repository
 public interface UserRepository extends CrudRepository <User, Integer>{
 
+	/**
+	 * @author Thitari
+	 * @return
+	 * this method follow convention specify by spring data jpa
+	 * Thus we do not have to specify any sql statement or any query 
+	 * and parse the parameter id in this method
+	 */
+	public Long countById(Integer id);
+
 }
