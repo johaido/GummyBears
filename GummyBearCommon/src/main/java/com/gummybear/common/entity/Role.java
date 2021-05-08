3e1c32146fdc3fbab0ec450980899f99cd31136b
Thitari Somboon <thitari.somboon@students.fhnw.ch>
2021-05-08 15:19:37 +0200

    hashcode() eqauls() for ID added

--- GummyBearCommon/src/main/java/com/gummybear/common/entity/Role.java
+++ GummyBearCommon/src/main/java/com/gummybear/common/entity/Role.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
+
 @Entity
 @Table(name = "roles")
 public class Role {
@@ -65,4 +67,34 @@ public class Role {
 	public String toString() {
 		return this.name;
 	}
+
+	//for ID
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	//for ID
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+
 }
