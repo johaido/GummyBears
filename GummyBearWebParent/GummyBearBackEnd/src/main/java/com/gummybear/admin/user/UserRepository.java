53ae85631c370f21c5e18d932fde73ab2b2b86dc
Thitari Somboon <thitari.somboon@students.fhnw.ch>
2021-05-08 14:54:01 +0200

    UserRepository class updated

--- GummyBearWebParent/GummyBearBackEnd/src/main/java/com/gummybear/admin/user/UserRepository.java
+++ GummyBearWebParent/GummyBearBackEnd/src/main/java/com/gummybear/admin/user/UserRepository.java
@@ -14,10 +14,11 @@ public interface UserRepository extends CrudRepository <User, Integer>{
 	
 	/**
 	 * @author Jonas
-	 
+	 * 
+	 * */
 	@Query("SELECT u FROM user u Where u.email = :email")
 	public User getUserByEmail(@Param("email") String email);
-	*/
+	
 	/**
 	 * @author Thitari
 	 * @return
