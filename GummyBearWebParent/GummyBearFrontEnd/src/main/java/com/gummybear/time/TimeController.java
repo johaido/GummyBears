3ece0fe3d697a3ac076615727215481f5bd82a41
osolga <olga.osipova@students.fhnw.ch>
2021-05-11 23:53:47 +0200

    Implemented TimeController and TimeService methods for checkin and checkout.

--- GummyBearWebParent/GummyBearFrontEnd/src/main/java/com/gummybear/time/TimeController.java
+++ GummyBearWebParent/GummyBearFrontEnd/src/main/java/com/gummybear/time/TimeController.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.gummybear.common.entity.User;
 
 @Controller
 public class TimeController {
@@ -11,19 +15,46 @@ public class TimeController {
 	@Autowired
 	private TimeService service;
 	
-	@GetMapping("/timeoverview")
-	public String listAll(Model model) {
-		List<Time> listTime = service.listAll();
-		model.addAttribute(listTime, listTime);
-		return "timeoverview";
+	/**
+	 * @author Olga
+	 * @param model
+	 * @param redirectAttributes
+	 * @return
+	 */
+	@PostMapping("/checkin")
+	public String checkIn(Model model, RedirectAttributes redirectAttributes) {
+		service.save(true);
+		redirectAttributes.addFlashAttribute("message", "The Check-in time was registered succesfully.");
+		return "redirect:/";
 	}
 	
-	@GetMapping("/timetracking")
-	public String timeTracking() {
-		return "timetracking";
+	/**
+	 * 
+	 * @author Olga
+	 * @param model
+	 * @param redirectAttributes
+	 * @return
+	 */
+	@PostMapping("/checkout")
+	public String checkOut(Model model, RedirectAttributes redirectAttributes) {
+		service.save(false);
+		redirectAttributes.addFlashAttribute("message", "The Check-out time was registered succesfully.");
+		return "redirect:/";
 	}
 	
 	
+	// Temporarily commented methods below as they were causing the error message
 	
+//	@GetMapping("/timeoverview")
+//	public String listAll(Model model) {
+//		List<Time> listTime = service.listAll();
+//		model.addAttribute(listTime, listTime);
+//		return "timeoverview";
+//	}
+	
+//	@GetMapping("/timetracking")
+//	public String timeTracking() {
+//		return "timetracking";
+//	}
 	
 }
--- GummyBearWebParent/GummyBearFrontEnd/src/main/java/com/gummybear/time/TimeService.java
+++ GummyBearWebParent/GummyBearFrontEnd/src/main/java/com/gummybear/time/TimeService.java
@@ -1,5 +1,41 @@
 package com.gummybear.time;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gummybear.common.entity.TimeStamp;
+import com.gummybear.common.entity.User;
+
+@Service
+@Transactional
 public class TimeService {
+	
+	@Autowired
+	private TimeRepository timeRepo;
+	
+	/**
+	 * Method to register check-in and check-out time in the DB.
+	 * Save year, month, day, hour and minute of the time stamp in the DB.
+	 * @author Olga
+	 */
+	public void save(boolean eventType) {
+		Date date = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		int year = calendar.get(Calendar.YEAR);
+		int month = (calendar.get(Calendar.MONTH) + 1); // counting from 0 (zero)
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		int hour = calendar.get(Calendar.HOUR_OF_DAY);
+		int minute = calendar.get(Calendar.MINUTE);
+		
+		// TODO: adjust eventType to true or false depending on whether it was check-in or check-out event.
+		// TODO: when user login id will be available it will be passed to the constructor to link time stamp to specific user
+		TimeStamp ts = new TimeStamp(year, month, day, hour, minute, eventType);
+		timeRepo.save(ts);
+	}
 
 }
